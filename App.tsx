86a6b4cf2c8239e782cc0c6574d5fe3d7b72fc3c
Dhinesh4668 <dhinesh4668@outlook.com>
2024-03-27 11:31:44 +0530

    razorpay test config

--- App.tsx
+++ App.tsx
@@ -8,6 +8,9 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Alert,
+  Animated,
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -24,6 +27,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import RazorpayCheckout from 'react-native-razorpay';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -31,6 +35,9 @@ type SectionProps = PropsWithChildren<{
 
 function Section({children, title}: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+
+ 
+
   return (
     <View style={styles.sectionContainer}>
       <Text
@@ -62,6 +69,41 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+   // test keys of rayzorpay 
+   const test_key = 'add your key '; //test key
+   const test_secrot_key = 'jsghhdfghsf';  //test seckert key
+ 
+   // handle the payment 
+   const handlePayment = () =>{
+    // seting the options
+    var options = {
+      description: 'Credits towards consultation',
+      image: 'https://i.imgur.com/3g7nmJC.jpg',
+      currency: 'INR',
+      key: test_key,
+      amount: '5000',
+      name: 'name',
+      order_id: '',//Replace this with an order_id created using Orders API.
+      prefill: {
+        email: 'testuser@example.com',
+        contact: '9797979797',
+        name: 'contact user'
+
+      },
+      theme: {color: '#53a20e'},
+      modal: {animation: true},
+      config: {display: {
+        language: 'tam'
+      }}
+    }
+    RazorpayCheckout.open(options).then((data) => {
+      // handle success
+      Alert.alert(`Success`, `${data.razorpay_payment_id}`);
+    }).catch((error) => {
+      // handle failure
+      alert(`Error: ${error.code} | ${error.description}`);
+    });
+   }
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
@@ -72,6 +114,7 @@ function App(): React.JSX.Element {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
+        <Button title="rayzor pay payment" onPress={handlePayment} />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
